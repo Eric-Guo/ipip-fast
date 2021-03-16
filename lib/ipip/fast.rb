ef84e9335ca5a3be1f905ede08389acf8c8c5673
Eric-Guo <eric.guocz@gmail.com>
2021-03-16 15:47:18 +0800

    bundle gem ipip-fast --no-exe --no-coc --no-ext --mit

---
+++ lib/ipip/fast.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "fast/version"
+
+module Ipip
+  module Fast
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/ipip/fast/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Ipip
+  module Fast
+    VERSION = "0.1.0"
+  end
+end
